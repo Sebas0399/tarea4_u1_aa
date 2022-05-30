ff5882772912286cc4730d437b886aa533e7c2a8
Sebas0399 <antony.aguay@gmail.com>
2022-05-30 08:18:26 -0500

    primera subida

---
+++ src/main/java/com/uce/edu/demo/Tarea4U1AaApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea4U1AaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea4U1AaApplication.class, args);
+	}
+
+}
